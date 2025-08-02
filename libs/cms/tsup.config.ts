8f196f74f0ae9d11deacd27b37967370023782be
Felix Mokross <felixmokross@gmail.com>
2025-08-02 14:24:17 +0200

    Separate labels export for use in client components

--- libs/cms/tsup.config.ts
+++ libs/cms/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: [
     "src/index.ts",
+    "src/common/labels.ts",
     "src/common/labels.client.ts",
     "src/fields/usages/usages-field.tsx",
   ],
