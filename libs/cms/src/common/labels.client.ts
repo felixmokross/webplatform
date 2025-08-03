8f5d64291e343e57ca89d3f410ad36307cded7a8
Felix Mokross <felixmokross@gmail.com>
2025-08-03 17:54:31 +0200

    Remove CMS lib again, need new setup

--- libs/cms/src/common/labels.client.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useTranslation } from "@payloadcms/ui";
-import { getLabelText, type LabelData } from "./labels";
-
-export function Label({ children }: { children: LabelData }) {
-  const { i18n } = useTranslation();
-  return getLabelText(children, i18n);
-}
