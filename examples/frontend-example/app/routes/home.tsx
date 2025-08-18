72f94cba416a42aa14c12179441b8fffe8294434
Felix Mokross <felixmokross@gmail.com>
2025-08-18 21:53:02 +0200

    Add frontend package dependency to frontend-example

--- examples/frontend-example/app/routes/home.tsx
+++ examples/frontend-example/app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { hello } from "@fxmk/frontend";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <Welcome />
+      {hello}
+    </>
+  );
 }
