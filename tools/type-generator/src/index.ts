e42acb0fe81e2f41252ae8c07b17791bf3cf48fc
Felix Mokross <felixmokross@gmail.com>
2025-08-19 21:00:44 +0200

    Simplify type generator, generate types for frontend

--- tools/type-generator/src/index.ts
+++ tools/type-generator/src/index.ts
@@ -1,6 +1,35 @@
 import { program } from "commander";
-import "./commands";
 
-program.name("type-generator");
+import { cmsPlugin } from "@fxmk/cms-plugin";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { sanitizeConfig } from "payload";
+import { generateTypes } from "payload/node";
 
-program.parse();
+program
+  .name("type-generator")
+  .argument("<outputFile>")
+  .action(async (outputFile) => {
+    let pluginConfig = cmsPlugin({
+      mediaS3Storage: {
+        accessKeyId: "",
+        bucket: "",
+        region: "",
+        secretAccessKey: "",
+      },
+    })({
+      db: mongooseAdapter({ url: false }),
+      // @ts-expect-error secret is not needed for type generation
+      secret: null,
+      typescript: {
+        outputFile,
+        declare: false,
+      },
+    });
+
+    if (pluginConfig instanceof Promise) {
+      pluginConfig = await pluginConfig;
+    }
+
+    generateTypes(await sanitizeConfig(pluginConfig));
+  })
+  .parse();
